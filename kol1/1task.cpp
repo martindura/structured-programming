3da7df983b1807bbc133b9075aa7f023bc898728
Martin Durakov <martindurakov@gmail.com>
2025-01-06 23:35:30 +0100

    solution for lab 9 task 3

--- kol1/1task.cpp
+++ kol1/1task.cpp
@@ -6,13 +6,48 @@
 #include <cctype>
 using namespace std;
 
+int isPali(char arr[], int start, int end) {
+    int counter = 0, i = start, j = end - 1;
+    bool flag = true;
+
+    while (i < end && j >= start) {
+        if (tolower(arr[i]) != tolower(arr[j])) {
+            flag = false;
+        }
+
+        i++;
+        j--;
+    }
+    if (flag) {
+        counter++;
+    }
+    return counter;
+}
+
 
 int main() {
-    const int max = 100;
-    char arr[max];
-    cin.getline(arr, max);
+    int n;
+    cin >> n;
+    cin.ignore();
+    while (n--) {
+        int counter = 0;
+        const int max = 100;
+        char arr[max];
+        int start = 0, end = 0;
+        cin.getline(arr, max);
+        int i = 0;
+        for (i = 0; arr[i] != '\0'; i++) {
+            if (isspace(arr[i])) {
+                end = i;
+                counter += isPali(arr, start, end);
+                start = end + 1;
+            }
+        }
+        end = i;
+        counter += isPali(arr, start, end);
 
-
+        cout << arr << ": " << counter << endl;
+    }
 
     return 0;
 }
